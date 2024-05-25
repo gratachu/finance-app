8de858e53dac234ee82ac365758e779d9eae1dcc
TatsuakiIto <gratachu9@gmail.com>
2024-05-25 19:33:41 +0900

    Spike: authors and books route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,24 +1,16 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    })
-  })
-  .get("hello/:test", (c) => {
-    const test = c.req.param("test")
-
-    return c.json({
-      message: "Hello Next.js!",
-      test: test,
-    })
-  })
+app.route("/authors", authors)
+app.route("/books", books)
 
 export const GET = handle(app)
 export const POST = handle(app)
