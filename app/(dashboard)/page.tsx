15f9017bd3d6e92267f12f6b8fcc69847e31833b
TatsuakiIto <gratachu9@gmail.com>
2024-05-27 21:33:43 +0900

    Add: header skeleton component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,8 @@ import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <UserButton afterSignOutUrl={"/"} />
+    <div>
+      Dashboard Page
+    </div>
   )
 }
