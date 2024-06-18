a677582f4cb11c9babf08866b7dda283c301642a
gratachu <akitatsu95.climbing@gmail.com>
2024-06-18 19:48:32 +0900

    Add: post method for create account

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -2,8 +2,9 @@ import {z} from "zod";
 
 import {useNewAccount} from "@/features/accounts/hooks/use-new-account";
 import {AccountForm} from "@/features/accounts/components/account-form";
-import {insertAccountSchema} from "@/db/schema";
+import {useCreateAccount} from "@/features/accounts/api/use-create-account";
 
+import {insertAccountSchema} from "@/db/schema";
 import {
   Sheet,
   SheetDescription,
@@ -21,8 +22,14 @@ type FormValues = z.input<typeof formSchema>
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount()
 
+  const mutation = useCreateAccount()
+
   const onSubmit = (values: FormValues) => {
-    console.log(values)
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose()
+      }
+    })
   }
 
   return (
@@ -31,7 +38,13 @@ export const NewAccountSheet = () => {
         <SheetHeader>
           New Account
         </SheetHeader>
-        <AccountForm onSubmit={onSubmit} disabled={false} />
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
         <SheetDescription>
           Create a new account to track your transactions.
         </SheetDescription>
