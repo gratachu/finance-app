277bb35aef676cf641deeecb0c6eb8f06dd13030
TatsuakiIto <gratachu9@gmail.com>
2024-07-25 19:46:47 +0900

    add: transaction schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,12 @@
-import {pgTable, text} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp
+} from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import {z} from "zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -7,6 +14,10 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 })
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions)
+}))
 
 export const insertAccountSchema = createInsertSchema(accounts)
 
@@ -17,4 +28,38 @@ export const categories = pgTable("categories", {
   userId: text("user_id").notNull(),
 })
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions)
+}))
+
 export const insertCategorySchema = createInsertSchema(accounts)
+
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id").references(() => accounts.id, {
+    onDelete: "cascade",
+  }).notNull(),
+  // MEMO: categoryを削除しても、transactionsは残しておきたい
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }).notNull(),
+})
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id]
+  }),
+  category: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id]
+  })
+}))
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+})
